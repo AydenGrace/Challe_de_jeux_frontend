2f057220b0d399ff5e7cd991ea8ed85789aea3db
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-16 16:28:09 +0200

    Change password

--- src/pages/Change_Password/Change_Password.jsx
+++ src/pages/Change_Password/Change_Password.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Change_Password.module.scss";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
