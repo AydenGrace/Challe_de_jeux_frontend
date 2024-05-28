1b3d3c1fc5bb960b6e16de7d92df5a8bf9833079
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-28 10:20:20 +0200

    test

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import Contact from "./components/contact/Contact";
 import Informations from "./components/informations/Informations";
 import Landing from "./components/landing/Landing";
 import Residents from "./components/residents/Residents";
 import { useNavigation } from "react-router-dom";
 import Loading from "../../components/Loading";
+import { UserContext } from "../../context/UserContext";
 
 export default function Homepage() {
   const { state } = useNavigation();
+  const { user } = useContext(UserContext);
   return state !== "idle" ? (
     <Loading />
   ) : (
