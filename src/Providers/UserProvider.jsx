ae1eb8b04c9bbb053c9ecb94a27f5114c5a47772
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-28 10:31:44 +0200

    test

--- src/Providers/UserProvider.jsx
+++ src/Providers/UserProvider.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
 
 export default function UserProvider({ children }) {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState();
 
   useEffect(() => {
     getLocalUser();
