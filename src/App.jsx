4f57e8aa31b6798018a918c06073dd12964365f4
Pierre-Alexandre <pa.ce.hepl@gmail.com>
2024-07-03 16:51:40 +0200

    Cookies

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,12 @@ function App() {
         <CookieConsent
           location="bottom"
           buttonText="Accepter"
+          declineCookieValue={true}
           declineButtonText="Refuser"
           cookieName="Challe_de_jeux_Cookie"
           style={{ background: "#e5dbc3", color:'#000' }}
           buttonStyle={{ backgroundColor: "#5d4431" ,color: "#fff", fontSize: "1rem" }}
+          declineButtonStyle={{ backgroundColor: "#5d4431" ,color: "#fff", fontSize: "1rem" }}
           expires={150}
         >
           Ce site internet utilise des cookies conformément à sa <a href="/privacy">Politique de confidentialité</a>.
