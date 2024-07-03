89a6af501f5b80b85103f1f4fd919aa1541e5ad1
Pierre-Alexandre <pa.ce.hepl@gmail.com>
2024-07-03 14:35:26 +0200

    Privacy link

--- src/pages/Legals/Privacy/Privacy.jsx
+++ src/pages/Legals/Privacy/Privacy.jsx
@@ -3,10 +3,17 @@ import styles from './Privacy.module.scss'
 
 export default function Privacy() {
   return (
-    <section className={`d-flex flex-column`}>
+    <div className={`d-flex flex-column`}>
         <div className={`${styles.headerSeparator}`}></div>
         <h1>Politique de confidentialité</h1>
+        <section className='d-flex flex-column justify-content-start'>
+            <h2>Identité du site Web</h2>
+            <p><strong>Nom :</strong> La Challe de Jeux</p>
+            <p><strong>Adresse :</strong> 19 rue des Arts, 59000 Lille, France</p>
+            <p><strong>Adresse en ligne :</strong> <a>www.challedejeux.fr</a></p>
+            <p><strong>Catégorie de site web :</strong> E-Commerce</p>
+        </section>
 
-    </section>
+    </div>
   )
 }
