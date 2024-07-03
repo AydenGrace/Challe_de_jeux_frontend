d3324201c7dbd5278e6d52dcb09bf421a9ef633c
Pierre-Alexandre <pa.ce.hepl@gmail.com>
2024-07-03 14:32:16 +0200

    Privacy link

--- src/pages/Legals/Privacy/Privacy.jsx
+++ src/pages/Legals/Privacy/Privacy.jsx
@@ -4,8 +4,9 @@ import styles from './Privacy.module.scss'
 export default function Privacy() {
   return (
     <section className={`d-flex flex-column`}>
+        <div className={`${styles.headerSeparator}`}></div>
         <h1>Politique de confidentialité</h1>
-        
+
     </section>
   )
 }
