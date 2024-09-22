99907bfcfab66eff12e98dac2301e4be1b2d00c9
Pierre-Alexandre <pa.ce.hepl@gmail.com>
2024-09-22 19:52:25 +0200

    aria label

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,13 +4,13 @@ import styles from "./Socials.module.scss";
 export default function Socials() {
   return (
     <div className={styles.container}>
-      <a aria-label='Facebook' href="https://www.facebook.com/?locale=fr_FR" target="_blank">
+      <a aria-labelledby="Facebook" href="https://www.facebook.com/?locale=fr_FR" target="_blank">
         <i title='Facebook' className="fa-brands fa-facebook-f fa-xl"></i>
       </a>
-      <a aria-label='Instagram' href="https://www.instagram.com/" target="_blank">
+      <a aria-labelledby="Instagram" href="https://www.instagram.com/" target="_blank">
         <i title="Instagram" className="fa-brands fa-instagram fa-xl"></i>
       </a>
-      <a aria-label='Tiktok' href="https://www.tiktok.com/fr/" target="_blank">
+      <a aria-labelledby="Tiktok" href="https://www.tiktok.com/fr/" target="_blank">
         <i title="Tiktok" className="fa-brands fa-tiktok fa-xl"></i>
       </a>
     </div>
